ea4c0fb389fa4df500f170ea2ae2da7980d373dc
Jhonny Quevedo <quevedojhonny@gmail.com>
2023-10-21 14:09:58 -0300

    tercer commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
-var precio = 400000
+var precio = 400000;
 
 var precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
 
-var number = document.querySelector(".cantidad")
+var number = document.querySelector(".cantidad");
 function suma(){
-    var numberFormated = parseInt(number.innerText)
-    number.innerText = numberFormated + 1
+    var numberFormated = parseInt(number.innerText);
+    number.innerText = numberFormated + 1;
 } 
 
 function resta(){
-    var numberFormated = parseInt(number.innerText)
+    var numberFormated = parseInt(number.innerText);
     if(numberFormated > 0){
-    number.innerText = numberFormated - 1
+    number.innerText = numberFormated - 1;
     }
 }
 
-var number1 = document.querySelector(".precio-inicial")
-var number2 = document.querySelector(".cantidad")
-var result = document.querySelector(".valor-total")
+var number1 = document.querySelector(".precio-inicial");
+var number2 = document.querySelector(".cantidad");
+var result = document.querySelector(".valor-total");
 
 function multiplicacion(){
-    var numberOneFormated = parseInt (number1.innerText)
-    var numberTwoFormated = parseInt (number2.innerText)
-    result.innerText = numberOneFormated * numberTwoFormated
+    var numberOneFormated = parseInt (number1.innerText);
+    var numberTwoFormated = parseInt (number2.innerText);
+    result.innerText = numberOneFormated * numberTwoFormated;
 }    
